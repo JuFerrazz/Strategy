ac3555a10c4fd480d498452a06faf447077016c8
Juliana Ferraz <154102521+JuFerrazz@users.noreply.github.com>
2024-10-01 22:42:20 -0300

    Add files via upload

---
+++ Disciplina.java
@@ -0,0 +1,40 @@
+package strategy;
+
+public class Disciplina {
+	
+	 private String nome;
+	    private double[] notas;
+	    private Calculo calculo;
+	    private double media;
+	    
+	    public Disciplina(Calculo calculo) {
+	        this.calculo = calculo;
+	    }
+
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+
+	    public void setNotas(double[] notas) {
+	        this.notas = notas;
+	    }
+
+	    public void calcularMedia() {
+	        this.media = calculo.calcularMedia(notas);
+	    }
+	    
+	    public String getNome() {
+	    	return nome;
+	    }
+
+	    public double getMedia() {
+	        return this.media;
+	    }
+
+	    public String getSituacao() {
+	        if (calculo instanceof Aritmetica) {
+	            return this.media >= 5.0 ? "Aprovado" : "Reprovado";
+	        } else  
+	            return this.media >= 7.0 ? "Aprovado" : "Reprovado";
+	        }
+}
